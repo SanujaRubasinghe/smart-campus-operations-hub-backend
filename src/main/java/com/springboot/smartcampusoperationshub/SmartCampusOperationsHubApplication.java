83c43f071d0627b0efe49bd80296af1cc47f3306
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2026-03-05 20:40:35 +0530

    initial commit

---
+++ src/main/java/com/springboot/smartcampusoperationshub/SmartCampusOperationsHubApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.smartcampusoperationshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusOperationsHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCampusOperationsHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/smartcampusoperationshub/SmartCampusOperationsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.smartcampusoperationshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusOperationsHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
