8fed998ff732791b144e6bae6278bb8caff7cfe8
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2026-03-23 13:24:39 +0530

    add booking exception handlers

---
+++ src/main/java/com/springboot/smartcampusoperationshub/exception/bookings/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.springboot.smartcampusoperationshub.exception.bookings;
+
+public class GlobalExceptionHandler extends RuntimeException {
+  public GlobalExceptionHandler(String message) {
+    super(message);
+  }
+}
