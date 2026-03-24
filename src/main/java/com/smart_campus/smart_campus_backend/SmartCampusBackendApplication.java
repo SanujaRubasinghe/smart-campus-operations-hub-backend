04c4a7a35d328cab4d741de9c9ca974568e9f086
Javid Mushtan <javidmushtan@gmail.com>
2026-03-24 21:11:24 +0530

    reconstruct pom.xml

---
+++ src/main/java/com/smart_campus/smart_campus_backend/SmartCampusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smart_campus.smart_campus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusBackendApplication.class, args);
+	}
+
+}
