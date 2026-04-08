eab4a4cc6afc48938c4dc2d4d86873fb9f9339d0
emawattson <rashalividyanjani@gmail.com>
2026-04-08 20:39:36 +0530

    Updated TicketCommentRepository for managing ticket comment records

--- src/main/java/com/springboot/smartcampusoperationshub/repository/TicketCommentRepository.java
+++ src/main/java/com/springboot/smartcampusoperationshub/repository/TicketCommentRepository.java
@@ -3,8 +3,5 @@ package com.springboot.smartcampusoperationshub.repository;
 import com.springboot.smartcampusoperationshub.model.TicketComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TicketCommentRepository extends JpaRepository<TicketComment, Long> {
-    List<TicketComment> findByTicketId(Long ticketId);
 }
