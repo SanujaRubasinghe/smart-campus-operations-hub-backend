6825154d8476462573d0aebfb9b1a747247b77b6
emawattson <rashalividyanjani@gmail.com>
2026-04-08 20:34:46 +0530

    Modified UpdateTicketDetailsRequest DTO used when updating ticket details

--- src/main/java/com/springboot/smartcampusoperationshub/dto/UpdateCommentRequest.java
+++ src/main/java/com/springboot/smartcampusoperationshub/dto/UpdateCommentRequest.java
@@ -1,17 +1,46 @@
 package com.springboot.smartcampusoperationshub.dto;
 
+import com.springboot.smartcampusoperationshub.model.UserRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public class UpdateStatusRequest {
+public class UpdateCommentRequest {
 
-    @NotBlank
-    private String status;
+    @NotBlank(message = "Comment text is required")
+    @Size(max = 1000, message = "Comment cannot exceed 1000 characters")
+    private String commentText;
 
-    public String getStatus() {
-        return status;
+    @NotBlank(message = "Requester name is required")
+    private String requesterName;
+
+    @NotNull(message = "Requester role is required")
+    private UserRole requesterRole;
+
+    public UpdateCommentRequest() {
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public String getRequesterName() {
+        return requesterName;
+    }
+
+    public void setRequesterName(String requesterName) {
+        this.requesterName = requesterName;
+    }
+
+    public UserRole getRequesterRole() {
+        return requesterRole;
+    }
+
+    public void setRequesterRole(UserRole requesterRole) {
+        this.requesterRole = requesterRole;
     }
 }
