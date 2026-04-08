023df82d846fbd5561a5e79dec2a8cec45d820aa
emawattson <rashalividyanjani@gmail.com>
2026-04-08 20:38:09 +0530

    ResourceNotFoundException used when requested data cannot be found

--- src/main/java/com/springboot/smartcampusoperationshub/exception/ResourceNotFoundException.java
+++ src/main/java/com/springboot/smartcampusoperationshub/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.springboot.smartcampusoperationshub.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
