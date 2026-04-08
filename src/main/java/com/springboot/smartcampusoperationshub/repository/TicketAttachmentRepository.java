05717ff013d5e0e8d7727aab81aa83e2dbc16d4e
emawattson <rashalividyanjani@gmail.com>
2026-04-08 20:38:54 +0530

    TicketAttachmentRepository used for storing and retrieving attachments

--- src/main/java/com/springboot/smartcampusoperationshub/repository/TicketAttachmentRepository.java
+++ src/main/java/com/springboot/smartcampusoperationshub/repository/TicketAttachmentRepository.java
@@ -3,9 +3,5 @@ package com.springboot.smartcampusoperationshub.repository;
 import com.springboot.smartcampusoperationshub.model.TicketAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TicketAttachmentRepository extends JpaRepository<TicketAttachment, Long> {
-    List<TicketAttachment> findByTicketId(Long ticketId);
-    long countByTicketId(Long ticketId);
 }
