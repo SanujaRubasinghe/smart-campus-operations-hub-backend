12e934c64f05f20021885ce98b922bcacfbd01ad
emawattson <rashalividyanjani@gmail.com>
2026-04-08 20:36:07 +0530

    Updated BadRequestException class used to handle invalid requests

--- src/main/java/com/springboot/smartcampusoperationshub/exception/BadRequestException.java
+++ src/main/java/com/springboot/smartcampusoperationshub/exception/BadRequestException.java
@@ -1,4 +1,7 @@
 package com.springboot.smartcampusoperationshub.exception;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
