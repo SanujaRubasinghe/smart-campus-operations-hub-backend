af19ca497d697e4a3bf938670edc868a635a3955
Javid Mushtan <javidmushtan@gmail.com>
2026-04-12 01:31:17 +0530

    global exceptions added previos mistakes

---
+++ src/main/java/com/smartcampus/smart_campus_backend/exception/TokenRefreshException.java
@@ -0,0 +1,11 @@
+package com.smartcampus.smart_campus_backend.exception;
+
+public class TokenRefreshException extends RuntimeException {
+    public TokenRefreshException(String message) {
+        super(message);
+    }
+
+    public TokenRefreshException(String token, String message) {
+        super(String.format("Failed for [%s]: %s", token, message));
+    }
+}
